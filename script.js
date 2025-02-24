49f54b6aec19c6b698f4331f3b6f3025c51a92bc
hordeiagri <142155405+hordeiagri@users.noreply.github.com>
2025-02-25 02:45:18 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Telegram Web App API - изменение цветов в зависимости от темы
-document.addEventListener("DOMContentLoaded", function() {
-    const tg = window.Telegram.WebApp;
-    tg.expand(); // Разворачиваем на весь экран
-
-    // Подключение к теме Telegram
-    document.body.style.backgroundColor = tg.themeParams.bg_color || "#f5f5f5";
-    document.body.style.color = tg.themeParams.text_color || "black";
-});
